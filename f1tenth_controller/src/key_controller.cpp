d5d111929a7b8d0f972db6bf97f3e93c63aa861b
pilyang <pilyang75@gmail.com>
2018-09-04 16:15:38 +0900

    Add files via upload

---
+++ f1tenth_controller/src/key_controller.cpp
@@ -0,0 +1,78 @@
+#include <unistd.h>
+#include <termios.h>
+#include <iostream>
+
+#include "ros/ros.h"
+#include "f1tenth_controller/racecar.h"
+
+
+char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "key_controller");
+    ros::NodeHandle nh;
+
+    ros::Publisher pub = nh.advertise<f1tenth_controller::racecar>("/racecar_param", 10);
+    ros::Rate loop_rate(50);
+
+    f1tenth_controller::racecar msg;
+    msg.velocity = 0;
+    msg.angle = 0;
+
+    char ch;
+
+    std::cout<<"  w"<<std::endl;
+    std::cout<<"a s d"<<std::endl;
+    std::cout<<"f : velocity->0"<<std::endl;
+    std::cout<<"x : angle->0"<<std::endl;
+
+    while(ros::ok())
+    {
+        ch = getch();
+        switch(ch)
+        {
+        case 'w':
+            msg.velocity += 1;
+            break;
+        case 'a':
+            msg.angle += 0.1;
+            break;
+        case 's':
+            msg.velocity -= 1;
+            break;
+        case 'd':
+            msg.angle -= 0.1;
+            break;
+        case 'f':
+            msg.velocity = 0;
+            break;
+        case 'x':
+            msg.angle = 0;
+            break;
+        }
+        
+
+        pub.publish(msg);
+        loop_rate.sleep();
+    }
+    return 0;
+}
---
+++ f1tenth_controller/src/sim_connector.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+#include "f1tenth_controller/racecar.h"
+
+
+class Connector
+{
+private:
+    ros::NodeHandle nh_cn;
+    ros::Publisher left_front_wheel_velocity_pub;
+    ros::Publisher right_front_wheel_velocity_pub;
+    ros::Publisher left_rear_wheel_velocity_pub;
+    ros::Publisher right_rear_wheel_velocity_pub;
+    ros::Publisher left_steering_hinghe_position_pub;
+    ros::Publisher right_steering_hinghe_position_pub;
+    ros::Subscriber racecar_sub;
+    std_msgs::Float64 velocity;
+    std_msgs::Float64 angle;
+public:
+    Connector(const ros::NodeHandle &h)
+    : nh_cn(h)
+    {
+        racecar_sub = nh_cn.subscribe("/racecar_param", 10, &Connector::subCallback, this);
+        left_front_wheel_velocity_pub = nh_cn.advertise<std_msgs::Float64>("/racecar/left_front_wheel_velocity_controller/command", 10);
+        right_front_wheel_velocity_pub = nh_cn.advertise<std_msgs::Float64>("/racecar/right_front_wheel_velocity_controller/command", 10);
+        left_rear_wheel_velocity_pub = nh_cn.advertise<std_msgs::Float64>("/racecar/left_rear_wheel_velocity_controller/command", 10);
+        right_rear_wheel_velocity_pub = nh_cn.advertise<std_msgs::Float64>("/racecar/right_rear_wheel_velocity_controller/command", 10);
+        left_steering_hinghe_position_pub = nh_cn.advertise<std_msgs::Float64>("/racecar/left_steering_hinge_position_controller/command", 10);
+        right_steering_hinghe_position_pub = nh_cn.advertise<std_msgs::Float64>("/racecar/right_steering_hinge_position_controller/command", 10);
+        ROS_INFO("start connector node");
+    }
+    void subCallback(const f1tenth_controller::racecar::ConstPtr& msg);
+    void pubCallback();
+};
+
+void Connector::subCallback(const f1tenth_controller::racecar::ConstPtr& msg)
+{
+    double temp_v, temp_a;
+    temp_v = (double)msg->velocity;
+    temp_a = (double)msg->angle;
+    this->velocity.data = temp_v;
+    this->angle.data = temp_a;
+    this->pubCallback();
+}
+
+void Connector::pubCallback()
+{
+    this->left_steering_hinghe_position_pub.publish(angle);
+    this->right_steering_hinghe_position_pub.publish(angle);
+    this->left_front_wheel_velocity_pub.publish(velocity);
+    this->right_front_wheel_velocity_pub.publish(velocity);
+    this->left_rear_wheel_velocity_pub.publish(velocity);
+    this->right_rear_wheel_velocity_pub.publish(velocity);
+    double temp_v, temp_a;
+    temp_v = this->velocity.data;
+    temp_a = this->angle.data;
+    ROS_INFO("send velocity : %f", temp_v);
+    ROS_INFO("send angle    : %f", temp_a);
+    std::cout<<std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "sim_connector");
+    ros::NodeHandle nh;
+
+    Connector obj(nh);
+    ros::spin();
+
+    return 0;
+}
